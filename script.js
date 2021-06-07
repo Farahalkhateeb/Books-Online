c263585df706bdeceb6d2604bf65a1510f98fce4
Farah96 <>
2021-06-07 13:53:21 +0000

    new script

--- script.js
+++ script.js
@@ -2,69 +2,21 @@ var userName = prompt("Enter your name ? ");
 
 console.log(userName);
 
-alert("this is the best website for you dear " + userName)
+alert("You are now in the right place " + userName)
+function book4you (    ){
+var book4you = prompt("How do you prefer to read your books? choose if online ,download ")
+while (book4you != "online" && book4you!= "download"){
 
-
-    //  book4you = prompt("please enter on of : online download");
-// }
-// book4you();
-// }
-// var number = prompt("How many book have you read on our website?")
-// function numofbook(number){
-//   var output="";
-// for (var i=0 ; i<number; i++){
-// output+="<img src='https://www.iconpacks.net/icons/2/free-opened-book-icon-3169-thumb.png'>";
-// }
-// document.write(output);
-
-
-
-
-document.write( "your name is " + userName + "              ")
-
-
-
-// var page = 200;
-
-
-// var userpage
-
-
-
-
-
-// var userpage
-
-
-
-// if (userpage <= 200) {
-
-
-// if (userName <= 25) {
-
-//   alert("its fantasic")
-// } else {
-//   alert("you have to read it")
-// }
-
-
-    // if(condition 1){
-    //   if true do this code 1
-    // }else if(condition 2 ) {
-    //   if true do this code 2
-    // }else {
-    //    if both conditions were false do this code 
-    // }
-
-//if (page <= 200) {
-  //alert("its fantastic")
- //else if(page > 200) {
-  //alert("you have to read it")
-//}else{
-  ///alert("welcome")
-
-
-
-
-
-
+      book4you = prompt("please enter on of : online download");
+}
+book4you();
+}
+var number = prompt("How many book have you read on our website?")
+function numofbook(number){
+  var output="";
+for (var i=0 ; i<number; i++){
+output+="<img src='https://www.iconpacks.net/icons/2/free-opened-book-icon-3169-thumb.png'>";
+}
+document.write(output);
+}
+numofbook(number);
